02a59bae54131d942e33f63ae34063a51806b2ec
AnastasiiaSleptsova <am89650731404@gmail.com>
2023-04-09 13:33:02 +0300

    fix codestyles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,8 +6,9 @@ const Header = (props) => {
   return <header className={classes.header}>
     <img className={classes.logo} src='https://i.pinimg.com/originals/60/9f/fb/609ffba0ced75161f5237042df955fed.png'></img>
     <div className={classes.loginBlock}>
-      {props.isAuth ? props.login
-      : <NavLink className={classes.text} to={'/login'}>Login </NavLink> }
+      {props.isAuth
+        ? props.login
+        : <NavLink className={classes.text} to={'/login'}>Login </NavLink>}
     </div>
   </header>
 }
