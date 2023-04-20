f908ce789a97bd97a10a8e49ebc30ee20993bebe
AnastasiiaSleptsova <am89650731404@gmail.com>
2023-04-20 13:34:47 +0300

    add api file

--- src/Components/Friends/FriendItem.jsx
+++ src/Components/Friends/FriendItem.jsx
@@ -3,11 +3,59 @@ import React from 'react';
 import AvatarPlug from '../../img/avatar1.webp';
 import { Link, } from 'react-router-dom';
 import axios from 'axios';
-
+import { usersAPI } from '../../api/api';
 
 const FriendsItem = ({ friend, toogleFollowing, forwardRef, toggleFollowingProgress, followingInProgress }) => {
 
     const correctSrc = friend.photos.small || AvatarPlug;
+    const handleClick = () => {
+        toggleFollowingProgress(true, friend.id);
+        if (friend.followed) {
+            usersAPI.unFollowOnUser(friend.id)
+                .then(resp => {
+                    if (resp.resultCode === 0) {
+                        toogleFollowing(friend.id);
+                    }
+                    toggleFollowingProgress(false, friend.id);
+                })
+        } else {
+            usersAPI.followOnUser(friend.id)
+                .then(resp => {
+                    if (resp.resultCode === 0) {
+                        toogleFollowing(friend.id);
+                    }
+                    toggleFollowingProgress(false, friend.id);
+                })
+        }
+
+        // axios.post(`https://social-network.samuraijs.com/api/1.0//follow/${friend.id}`, {}, {
+        //     withCredentials: true,
+        //     headers: {
+        //         "API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
+        //     }
+        // })
+        // .then(resp => {
+        //         if (resp.data.resultCode == 0) {
+        //             toogleFollowing(friend.id);
+        //         }
+        //         toggleFollowingProgress(false, friend.id);
+        //     })
+        // toggleFollowingProgress(true, friend.id);
+
+        // axios.delete(`https://social-network.samuraijs.com/api/1.0//follow/${friend.id}`, {
+        //     withCredentials: true,
+        //     headers: {
+        //         "API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
+        //     }
+        // })
+        //     .then(resp => {
+        //         if (resp.data.resultCode == 0) {
+        //             toogleFollowing(friend.id);
+        //         }
+        //         toggleFollowingProgress(false, friend.id);
+        //     })
+    }
+
     return (
         <div className={classes.friend} ref={forwardRef}>
             <Link
@@ -23,36 +71,7 @@ const FriendsItem = ({ friend, toogleFollowing, forwardRef, toggleFollowingProgr
                 <button
                     className={classes.dropdown}
                     disabled={followingInProgress.some(id => id === friend.id)}
-                    onClick={() => {
-                        toggleFollowingProgress(true, friend.id);
-                        axios.post(`https://social-network.samuraijs.com/api/1.0//follow/${friend.id}`, {}, {
-                            withCredentials: true,
-                            headers: {
-                                " API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
-                            }
-                        })
-                            .then(resp => {
-                                if (resp.data.resultCode == 0) {
-                                    toogleFollowing(friend.id);
-                                }
-                                toggleFollowingProgress(false, friend.id);
-                            })
-                        toggleFollowingProgress(true, friend.id);
-
-                        axios.delete(`https://social-network.samuraijs.com/api/1.0//follow/${friend.id}`, {
-                            withCredentials: true,
-                            headers: {
-                                " API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
-                            }
-                        })
-                            .then(resp => {
-                                if (resp.data.resultCode == 0) {
-                                    toogleFollowing(friend.id);
-                                }
-                                toggleFollowingProgress(false, friend.id);
-                            })
-
-                    }}
+                    onClick={handleClick}
                 >
                     {friend.followed ? 'Unfollow' : 'Follow'}
                 </button>
@@ -62,3 +81,23 @@ const FriendsItem = ({ friend, toogleFollowing, forwardRef, toggleFollowingProgr
 }
 
 export default FriendsItem;
+
+
+// onClick={() => {
+//     toggleFollowingProgress(true, friend.id);
+//     usersAPI.getUsers2(friend.id).then(resp => {
+//             if (resp.data.resultCode == 0) {
+//                 toogleFollowing(friend.id);
+//             }
+//             toggleFollowingProgress(false, friend.id);
+//         })
+//     toggleFollowingProgress(true, friend.id);
+
+//     usersAPI.getUsers3(friend.id).then(resp => {
+//             if (resp.data.resultCode == 0) {
+//                 toogleFollowing(friend.id);
+//             }
+//             toggleFollowingProgress(false, friend.id);
+//         })
+
+// }}
--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create (
         withCredentials: true,
         baseURL: `https://social-network.samuraijs.com/api/1.0/`,
         headers: {
-            " API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
+            "API-KEY": "cfc6a4aa-5aaa-4793-bd4b-eea8791afc40"
         },
     }
 )
@@ -23,14 +23,30 @@ export const usersAPI = {
             return resp.data;
         });
     },
-    getUsers2(id) {
-        return instance.post(`/follow/${id}`)
-            .then(resp => {
-                return resp.data;
-            });
+    followOnUser(userID) {
+        return instance.post(`follow/${userID}`)
+        .then(resp => {
+            return resp.data;
+        });
     },
+    unFollowOnUser(userID) {
+        return instance.delete(`follow/${userID}`)
+        .then(resp => {
+            return resp.data;
+        });
+    }
+    // getUsers2(id) {
+    //     return instance.post(`/follow/${id}`)
+    //         .then(resp => {
+    //             return resp.data;
+    //         });
+    // },
+    // getUsers3(id) {
+    //     return instance.delete(`/follow/${id}`)
+    //         .then(resp => {
+    //             return resp.data;
+    //         });
+    // },
 }
-
-
 
 
